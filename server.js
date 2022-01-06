37f333a50b28587c1cbe2728ad4923e0779236d9
Mike Melgren <mikemelgren@gmail.com>
2022-01-06 11:06:43 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const errorHandler = require('_middleware/error-handler');
+const request = require('request');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// allow cors requests from any origin with credentials
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+
+//proxy for requests to XML/RSS podcast feeds
+app.get('/feeds', (req, res) => {
+    request(
+        { url: req.query.url },
+        (error, response, body) => {
+            if (error || response.statusCode !== 200) {
+                return res.status(500).json({
+                    type: 'error', message:
+                        err.message
+                })
+            }
+            res.set('Content-Type', 'application/rss+xml')
+            res.send(Buffer.from(body))
+        })
+})
+
+// api routes
+app.use('/accounts', require('./accounts/accounts.controller'));
+
+// swagger docs route
+app.use('/api-docs', require('_helpers/swagger'));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+app.listen(port, () => {
+    console.log('Server listening on port ' + port);
+});
